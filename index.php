6770a6d09ddc21ab7f4b77c116762f69e4eb4f50
JAL1122 <159782493+JAL1122@users.noreply.github.com>
2024-10-12 09:51:30 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Encuesta</title>
+    <link rel="stylesheet" href="estilo.css">
+  <center><img src="logito.png" alt=""></center>
+</head>
+<body>
+    <h2 class="As">Comunícanos tu experiencia </h2>
+<section id="encuesta">
+
+
+<!--El siguiente codigo se creara luego de forma dinamica por cada pregunta -->
+<div class="contenedor-pregunta" id="0">
+    <h2>1- Tipo de cliente.</h2>
+    <div class="opciones">
+  <label for="" class="100">
+            <input type="radio" name="p0 " onclick="seleccionar(0,0)">
+            <span>Cliente nuevo</span>
+            
+            <span id="p00"></span>
+        </label>
+        <label for="" class="101">
+            <input type="radio" name="p0 " onclick="seleccionar(0,1)">
+            <span>Soy cliente</span>
+            
+            <span id="p01"></span>
+        </label>
+        
+        </label>
+       
+    </div>
+
+</div>
+
+<div class="contenedor-pregunta" id="0">
+    <h2>2- Como clasificarias el proceso de gestionar la renta. </h2>
+    <div class="opciones">
+  <label for="" class="100">
+            <input type="radio" name="p0 " onclick="seleccionar(0,0)">
+            <span>Muy Facil</span>
+            
+            <span id="p00"></span>
+        </label>
+        <label for="" class="101">
+            <input type="radio" name="p0 " onclick="seleccionar(0,1)">
+            <span>Regular</span>
+            
+            <span id="p01"></span>
+        </label>
+        <label for="" class="102">
+            <input type="radio" name="p0 " onclick="seleccionar(0,2)">
+            <span>Dificil</span>
+            
+            <span id="p03"></span>
+            </label>
+          
+       
+    </div>
+
+</div>
+
+
+<div class="contenedor-pregunta" id="0">
+    <h2>2- Como clasificar la atencion brindada por el representante que le atendió.</h2>
+    <div class="opciones">
+  <label for="" class="100">
+            <input type="radio" name="p0 " onclick="seleccionar(0,0)">
+            <span>Muy Facil</span>
+            
+            <span id="p00"></span>
+        </label>
+        <label for="" class="101">
+            <input type="radio" name="p0 " onclick="seleccionar(0,1)">
+            <span>Regular</span>
+            
+            <span id="p01"></span>
+        </label>
+        <label for="" class="102">
+            <input type="radio" name="p0 " onclick="seleccionar(0,2)">
+            <span>Dificil</span>
+            
+            <span id="p03"></span>
+            </label>
+          
+       
+    </div>
+
+</div>
+</section>
+
+
+
+</body>
+</html>
